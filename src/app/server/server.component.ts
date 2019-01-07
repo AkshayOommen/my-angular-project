8dd802d3a85ce911e0b0320e4dc5d70860c42b44
AkshayOommen <axeabraham@hotmail.com>
2019-01-07 14:12:31 +0530

    Added two new components - server and servers

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styles: [`
+        p {
+            color: red;
+            font-weight: bold;
+        }
+    `]
+})
+export class ServerComponent {
+
+}
